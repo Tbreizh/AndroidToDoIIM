a1f6984c8c1a538ad754a97b4272ad2aea20064d
GUILLO Tristan <tg180757@IMAC-E309-15.local>
2020-02-24 14:56:17 +0100

    display task

--- app/src/main/java/com/tristang/todo/taskList/TaskListFragment.kt
+++ app/src/main/java/com/tristang/todo/taskList/TaskListFragment.kt
@@ -4,9 +4,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tristang.todo.R
+import kotlinx.android.synthetic.main.fragment_task_list.*
+
 
 class TaskListFragment : Fragment() {
+
+    private val taskList = listOf("Task 1", "Task 2", "Task 3")
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,4 +22,12 @@ class TaskListFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
         return rootView
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recycle_view.adapter = TaskListAdapter(taskList)
+        recycle_view.layoutManager = LinearLayoutManager(activity)
+
+    }
+
 }
